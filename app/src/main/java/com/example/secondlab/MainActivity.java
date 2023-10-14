614e342ab352e0766f97965fc651080504988d00
Mykyta Berezuiev <nikbere17@gmail.com>
2023-10-14 11:51:31 +0300

    Added Button click code, spinner and created new Java class

--- app/src/main/java/com/example/secondlab/MainActivity.java
+++ app/src/main/java/com/example/secondlab/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.secondlab;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Spinner spinner;
+    private Button button;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        spinner = findViewById(R.id.spinner);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.count_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = editText.getText().toString();
+                String selectedOption = spinner.getSelectedItem().toString();
+
+                int count = WordCharacterCounter.getCount(inputText, selectedOption);
+                textView.setText("Count: " + count);
+            }
+        });
     }
 }
